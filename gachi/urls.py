2137fbb49d6cdd959750f99cf39cb2ee5d4d1218
Mirim Kim <mirimkim00@gmail.com>
2024-10-30 23:46:24 -0400

    remove nesting data in landing page

--- gachi/urls.py
+++ gachi/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import test_supabase_connection
+from .views import landing_page_supabase_connection
 
 urlpatterns = [
-    path('test-supabase/', test_supabase_connection, name='test_supabase'),
+    path('landing-page-supabase/', landing_page_supabase_connection, name='landing_page_supabase'),
 ]
--- gachi/views.py
+++ gachi/views.py
@@ -13,23 +13,47 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_API_KEY)
 # Test view to fetch data from Supabase
 
 @api_view(['GET'])
-def test_supabase_connection(request):
+def landing_page_supabase_connection(request):
     # Get data from Program table
     response_program = supabase.table('Program').select('*').order('status').execute()
     programs = response_program.data
 
     # Get Mentors data from User table
-    response_user = supabase.table('User').select('*, Industry!inner(*)').eq('userTypeId', '74ae0cb7-e4bf-472e-a8a3-e94d895028e5').execute()
-    users = response_user.data
+    response_mentor = supabase.table('User').select('*, Industry!inner(*)').eq('userTypeId', '74ae0cb7-e4bf-472e-a8a3-e94d895028e5').execute()
+    mentors = response_mentor.data
+
+    flattened_mentors = []
+    for mentor in mentors:
+        industry_data = mentor.pop('Industry', {})
+        industryTitle = industry_data.get('title', None)
+
+        flattened_mentor = {
+            **mentor,
+            'industryTitle': industryTitle
+        }
+        flattened_mentors.append(flattened_mentor)
 
     # Get data from Review table
     response_review = supabase.table('Review').select("*, User!inner(*, Industry!inner(title))").execute()
     reviews = response_review.data
 
+    flattened_reviews = []
+    for review in reviews:
+        user_data = review.pop('User', {})
+        industry_data = user_data.pop('Industry', {})
+        industryTitle = industry_data.get('title', None)
+
+        flattened_review = {
+            **review,
+            **user_data,
+            'industryTitle': industryTitle
+        }
+        flattened_reviews.append(flattened_review)
+
     response = {
         'programs':programs,
-        'users': users,
-        'reviews': reviews,
+        'mentors': flattened_mentors,
+        'reviews': flattened_reviews,
     }
 
     return JsonResponse(response, safe=False)
